d61c42cfdea5cfaf98f2bb798fa08e3a17efdf9a
Stefan Templin <kuche@templinland.de>
2016-05-05 00:00:43 +0200

    Logitech Attack 3 Joystick zur Drohnensteuerung
    Verwende die native gamepad-node-Bibliothek um verschiedene Gamepads/Joysticks anzusteuern. Die Cylon-Bibliothekt (die sich nach intesiver Code-Einsicht als schlecht programmierten Wrapper entpuppte...) performt nicht.

---
+++ gamepad.js
@@ -0,0 +1,95 @@
+
+/* Ermöglicht Zugriff auf den Drohnenroboter.
+   Dieser ist der Einfachheit halber immer mit 'r' benannt.
+   Drohne ist also d.drone
+*/
+var Drone = require('./drone');
+var r = Drone.getAndActivateDrone();
+
+var gamepad = require("gamepad");
+
+// Initialize the library
+gamepad.init()
+
+// List the state of all currently attached devices
+for (var i = 0, l = gamepad.numDevices(); i < l; i++) {
+  console.log(i, gamepad.deviceAtIndex());
+}
+
+// Create a game loop and poll for events
+setInterval(gamepad.processEvents, 16);
+// Scan for new gamepads as a slower rate
+setInterval(gamepad.detectDevices, 500);
+
+// Listen for move events on all gamepads
+gamepad.on("move", function (id, axis, value) {
+  switch(axis){
+    case 0:
+      if(value > 0.01){
+        value = Math.round(value * 100);
+        console.log("moving right by: " + value);
+        r.drone.right(value);
+      }else if(value < -0.01){
+        value = Math.round(value * -100);
+        console.log("moving left by: " + value);
+        r.drone.left(value);
+      }else{
+        //r.drone.hover();
+      }
+      break;
+    case 1:
+      if(value > 0.01){
+        value = Math.round(value * 100);
+        console.log("moving backward by: " + value);
+        r.drone.backward(value);
+      }else if(value < -0.01){
+        value = Math.round(value * -100);
+        console.log("moving forward by: " + value);
+        r.drone.forward(value);
+      }else{
+        //r.drone.stop();
+      }
+      break;
+    default: {}
+  }
+
+  /*console.log("move", {
+    id: id,
+    axis: axis,
+    value: value,
+  });*/
+});
+
+// Listen for button up events on all gamepads
+gamepad.on("up", function (id, num) {
+
+  switch(num) {
+   case 0:
+       console.log("takeoff!");
+       r.drone.takeOff();
+       break;
+   case 1:
+       console.log("hovering...");
+       r.drone.stop();
+       break;
+   case 2:
+       console.log("landing...");
+       r.drone.land();
+       break;
+   default:
+       r.drone.emergency();
+}
+
+  console.log("up", {
+    id: id,
+    num: num,
+  });
+});
+
+// Listen for button down events on all gamepads
+gamepad.on("down", function (id, num) {
+  console.log("down", {
+    id: id,
+    num: num,
+  });
+});
--- keyboard.js
+++ keyboard.js
@@ -3,14 +3,14 @@
 var Cylon = require('cylon');
 
 /* Ermöglicht Zugriff auf den Drohnenroboter.
-   Dieser ist der Einfachheit halber immer mit 'd' benannt.
+   Dieser ist der Einfachheit halber immer mit 'r' benannt.
    Drohne ist also d.drone
 */
 var Drone = require('./drone');
 
 //====================== CODE ======================
 
-var d = Drone.getAndActivateDrone();
+var r = Drone.getAndActivateDrone();
 
 Cylon.robot({
   connections: {
@@ -24,53 +24,53 @@ Cylon.robot({
   work: function(k) {
     k.keyboard.on('t', function(key) {
       console.log("TAKEOFF");
-      d.drone.takeOff();
+      r.drone.takeOff();
       //after((5).seconds(), bebop.drone.land);
     });
 
     k.keyboard.on('space', function(key) {
       console.log("EMERGENCY!");
-      d.drone.emergency();
+      r.drone.emergency();
     });
 
     k.keyboard.on('l', function(key) {
       console.log("LAND!");
-      d.drone.land();
+      r.drone.land();
     });
 
     k.keyboard.on('e', function(key) {
       console.log("UP");
-      d.drone.up(10);
+      r.drone.up(10);
     });
 
     k.keyboard.on('q', function(key) {
       console.log("DOWN");
-      d.drone.down(10);
+      r.drone.down(10);
     });
 
     k.keyboard.on('w', function(key) {
       console.log("FORWARD");
-      bebop.drone.forward(20);
+      r.drone.forward(20);
     });
 
     k.keyboard.on('s', function(key) {
       console.log("BACKWARD");
-      d.drone.backward(20);
+      r.drone.backward(20);
     });
 
     k.keyboard.on('a', function(key) {
       console.log("LEFT!");
-      d.drone.left(20);
+      r.drone.left(20);
     });
 
     k.keyboard.on('d', function(key) {
       console.log("RIGHT!");
-      d.drone.right(20);
+      r.drone.right(20);
     });
 
     k.keyboard.on('keyup', function(key){
       console.log("STOP -> HOVER");
-      d.drone.stop();
+      r.drone.stop();
     });
   }
 }).start();
